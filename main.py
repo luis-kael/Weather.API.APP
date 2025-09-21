86b9ffa55f022330d6f89cf31e1c35ab75c16c08
luis-kael <sithukaungsett2001@gmail.com>
2025-09-21 16:02:48 +0630

    day2

--- main.py
+++ main.py
@@ -14,7 +14,76 @@ class WeatherApp(QWidget):
         self.temperature_label = QLabel("70°F", self)
         self.emoji_label = QLabel("☀️", self)
         self.description_label = QLabel("Sunny", self)
-                
+        self.initUI()
+        
+        
+    def initUI(self):
+        self.setWindowTitle("Weather App")
+        
+        vbox = QVBoxLayout()
+        
+        vbox.addWidget(self.city_label)
+        vbox.addWidget(self.city_input)
+        vbox.addWidget(self.get_weather_button)
+        vbox.addWidget(self.temperature_label)
+        vbox.addWidget(self.emoji_label)
+        vbox.addWidget(self.description_label)
+        
+        self.setLayout(vbox)
+        
+        self.city_label.setAlignment(Qt.AlignCenter)
+        self.city_input.setAlignment(Qt.AlignCenter)
+        self.temperature_label.setAlignment(Qt.AlignCenter)
+        self.emoji_label.setAlignment(Qt.AlignCenter)
+        self.description_label.setAlignment(Qt.AlignCenter)
+
+        self.city_label.setObjectName("city_label")
+        self.city_input.setObjectName("city_input")
+        self.get_weather_button.setObjectName("get_weather_button")
+        self.temperature_label.setObjectName("temperature_label")
+        self.emoji_label.setObjectName("emoji_label")
+        self.description_label.setObjectName("description_label")
+
+        self.setStyleSheet("""
+             QLabel, QPushButton{
+                 font-family: calibri;
+             }
+             QLabel#city_label{
+                 font-size: 40px;
+                 font-style: italic;
+             }
+             QLineEdit#city_input{
+                 font-size: 40px;
+             } 
+             QPushButton#get_weather_button{
+                 font-size: 30px;
+                 font-weight: bold;
+             }
+             QLabel#temperature_label{
+                 font-size: 75px;
+             } 
+             QLabel#emoji_label{
+                 font-size: 100px;
+                 font-family: Segoe UI emoji;
+             }
+             QLabel#description_label{
+                 font-size: 50px;
+             }          
+                           """)
+        
+    def get_weather(self):
+            pass
+        
+    def display_error(self, message):
+            pass
+        
+    def display_weather(self, data):
+            pass
+        
+        
+        
+        
+        
 if __name__ == "__main__":
     app = QApplication(sys.argv)
     weather_app = WeatherApp()
